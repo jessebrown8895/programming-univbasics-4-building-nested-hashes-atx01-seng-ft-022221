687e4978e6fd0c5c329147d5b40ae477c61cfd78
Jesse Brown <brownjesse8895@yahoo.com>
2021-02-03 02:42:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,9 +4,9 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  { :railroads => { :pieces = 4,
-    :rent_in_dollars = {},
-    :names = {}
+  { :railroads => { :pieces => 4,
+    :rent_in_dollars => {},
+    :names => {}
   }
   end
 
